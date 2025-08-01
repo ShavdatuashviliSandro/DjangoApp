c9d14f1e0778249f9501fb6077693dc2e6490f4b
Sandro Shavdatuashvili <sandro.shav@gmail.com>
2025-08-02 02:13:31 +0400

    Added show method structure

--- support_hub/men_hub/views.py
+++ support_hub/men_hub/views.py
@@ -3,3 +3,6 @@ from django.http import HttpResponse
 
 def index(request):
     return HttpResponse('Hello, you are on mens side!')
+
+def show(request, post_id):
+    return HttpResponse(f"This is a post with ID {post_id}!")
