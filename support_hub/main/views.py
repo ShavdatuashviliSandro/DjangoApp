c47091fdc3d15f3c76fe1be344afc95a268f642a
Sandro Shavdatuashvili <sandro.shav@gmail.com>
2025-08-04 02:12:14 +0400

    Added show page

--- support_hub/main/views.py
+++ support_hub/main/views.py
@@ -5,4 +5,7 @@ from django.templatetags.static import static
 def index(request):
     logo_url = static('images/heart.svg')
 
-    return render(request, 'main/index.html', {'range': range(5), 'logo_url': logo_url})
+    return render(request, 'main/index.html', {'range': range(5), 'logo_url': logo_url, 'id': 1})
+
+def show(request, post_id):
+    return render(request, 'main/show.html', {'post_id': post_id})
