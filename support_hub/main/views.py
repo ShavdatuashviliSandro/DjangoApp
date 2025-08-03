b07521a81c3133ab995776a653857a50ca065244
Sandro Shavdatuashvili <sandro.shav@gmail.com>
2025-08-04 01:10:25 +0400

    Added index page structure

--- support_hub/main/views.py
+++ support_hub/main/views.py
@@ -1,4 +1,8 @@
 from django.shortcuts import render
+from django.templatetags.static import static
+
 
 def index(request):
-    return render(request, 'main/index.html', {'description': 'some description'})
+    logo_url = static('images/heart.svg')
+
+    return render(request, 'main/index.html', {'range': range(5), 'logo_url': logo_url})
