8d479e23ffd6b80837a53a69054481563baef444
Sandro Shavdatuashvili <sandro.shav@gmail.com>
2025-08-06 01:00:14 +0400

    Added migrations

--- support_hub/main/models.py
+++ support_hub/main/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
-# Create your models here.
+
+class Post(models.Model):
+    title = models.CharField(max_length=255)
+    content = models.TextField(null=False, blank=False)
+    author = models.CharField(max_length=255)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    mark_for_deletion = models.BooleanField(default=False)
+
+class Comment(models.Model):
+    content = models.TextField(null=False, blank=False)
+    author = models.CharField(max_length=255)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    mark_for_deletion = models.BooleanField(default=False)
