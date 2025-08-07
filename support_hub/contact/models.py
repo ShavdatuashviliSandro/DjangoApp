f6b47306fab5265f3a0b12bb49e654df3d18860c
Sandro Shavdatuashvili <sandro.shav@gmail.com>
2025-08-08 02:30:03 +0400

    Added comment form

--- support_hub/contact/models.py
+++ support_hub/contact/models.py
@@ -4,3 +4,7 @@ class Contact(models.Model):
     name = models.CharField(max_length=100, blank=False, null=False)
     email = models.CharField(max_length=100, blank=False, null=False)
     message = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    is_answered = models.BooleanField(default=False)
+    mark_for_deletion = models.BooleanField(default=False)
