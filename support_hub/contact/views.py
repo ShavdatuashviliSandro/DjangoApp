b45fdcbc5110594dbf2f24b4115c2aca6eac23bf
Sandro Shavdatuashvili <sandro.shav@gmail.com>
2025-08-10 00:12:36 +0400

    Did forms with django forms

--- support_hub/contact/views.py
+++ support_hub/contact/views.py
@@ -5,9 +5,7 @@ from .forms import ContactForm
 def create(request):
     form = ContactForm(request.POST or None)
     if request.method == 'POST' and form.is_valid():
-        Contact.objects.create(name=request.POST.get('name', '').strip(),
-                               email=request.POST.get('email', '').strip(),
-                               message=request.POST.get('message', '').strip())
-
+        Contact.objects.create(**form.cleaned_data)
         return render(request, 'create.html', {'form': ContactForm()})
+
     return render(request, 'create.html', {'form': form})
