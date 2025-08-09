ee84f23770b4bee754a14ca6aae561c97b66c9f4
Sandro Shavdatuashvili <sandro.shav@gmail.com>
2025-08-09 22:45:44 +0400

    Added validations

--- support_hub/contact/views.py
+++ support_hub/contact/views.py
@@ -1,9 +1,19 @@
+from django.core.exceptions import ValidationError
 from django.shortcuts import render
 from contact.models import Contact
 
 def create(request):
     if request.method == 'POST':
-        Contact.objects.create(name=request.POST['name'], email=request.POST['email'], message=request.POST['message'])
-        return render(request, 'create.html', {'a': 1})
-    else:
-        return render(request, 'create.html', {'a': 1})
+
+        contact = Contact(name=request.POST.get('name', '').strip(),
+                          email=request.POST.get('email', '').strip(),
+                          message=request.POST.get('message', '').strip())
+        try:
+            contact.full_clean()
+            contact.save()
+            return render(request, 'create.html')
+        except ValidationError as e:
+            return render(request, 'create.html',
+                          {'errors': e})
+
+    return render(request, 'create.html')
