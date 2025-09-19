9f4179b88c384f5fa3cc00aee5d969409500a877
Sandro Shavdatuashvili <sandro.shav@gmail.com>
2025-09-19 22:25:45 +0400

    Test beautify lines

--- support_hub/main/views.py
+++ support_hub/main/views.py
@@ -5,9 +5,10 @@ from django.templatetags.static import static
 def index(request):
     logo_url = static('images/heart.svg')
 
-    return render(request, 'main/index.html',{
+    return render(request, 'main/index.html', {
         'range': range(5), 'logo_url': logo_url, 'id': 1
     })
+
 
 def show(request, post_id):
     return render(request, 'main/show.html', {
